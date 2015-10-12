b334d4bfed81325c3a1982891eaa90191d3e9cfa
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-10-12 17:33:03 +0900

    adding content to /bands

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -2,12 +2,59 @@ import Ember from 'ember';
 
 var Band = Ember.Object.extend({
   name: '',
+  language: '',
+
+  slug: Ember.computed('name', function () {
+    return this.get('name').dasherize();
+  }.property('name')),
+
+  site: Ember.computed('slug', 'language', function () {
+    return 'http://localhost:4200/' + this.get('slug' + '.' + this.get('language'));
+  }.property('site', 'language'))
 });
 
-var ledZeppelin = Band.create({name: 'Led Zeppelin'});
-var pearlJam = Band.create({name: 'Pearl Jam'});
-var fooFighters = Band.create({name: 'Foo Fighters'});
-var bands = [ledZeppelin, pearlJam, fooFighters];
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
+var blackDog = Song.create({
+  title: 'Black God',
+  band: 'Led Zeppelin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var daughter = Song.create({
+  title: 'Daughter',
+  band: 'Pearl Jam',
+  rating: 5
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var BandsCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['name:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties'),
+});
+
+var ledZeppelin = Band.create({name: 'Led Zeppelin', songs: [blackDog]});
+var pearlJam = Band.create({name: 'Pearl Jam', songs: [daughter, yellowLedbetter]});
+var fooFighters = Band.create({name: 'Foo Fighters', songs: [pretender]});
+
+var bands = BandsCollection.create();
+bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
 
 export default Ember.Route.extend({
   model: function () {
